c4bf08701be5d1e0b0512e17e01a63cdf19f3a9f
ThiagoFBastos <thiagobastos1705@gmail.com>
2023-12-21 19:00:15 -0300

    fix:generosController,musicasController,views/filme/perfil

--- controllers/generosController.js
+++ controllers/generosController.js
@@ -1,8 +1,12 @@
 const Genero = require('../models/Genero');
 
-exports.cadastra = function(req, res) {
-	let genero = {};
-	genero.nome = req.body.nome.trim();
-	Genero.create(genero);
-	res.redirect('/developer/genero/add');
+exports.cadastra = function(req, res, next) {
+    try{
+	    let genero = {};
+	    genero.nome = req.body.nome.trim();
+	    Genero.create(genero);
+	    res.redirect('/developer/genero/add');
+    } catch(err) {
+        next(err);
+    }
 };
--- controllers/musicasController.js
+++ controllers/musicasController.js
@@ -1,11 +1,15 @@
 const {Op} = require('sequelize');
 const Musica = require('../models/Musica');
 
-exports.cadastroMusica = function(req, res) {
-	let musica = {};
-	musica.titulo = req.body.titulo.trim();
-	musica.youtube = req.body.youtube.trim();
-	musica.artista = req.body.artista.trim();
-	Musica.create(musica);
-	res.redirect('/developer/musica/add');
+exports.cadastroMusica = function(req, res, next) {
+    try {
+	    let musica = {};
+	    musica.titulo = req.body.titulo.trim();
+	    musica.youtube = req.body.youtube.trim();
+	    musica.artista = req.body.artista.trim();
+	    Musica.create(musica);
+	    res.redirect('/developer/musica/add');
+    } catch(err) {
+        next(err);
+    }
 };
